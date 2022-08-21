d03248a630ce27634c4ae514bcca36759376a255
Abner J Pelisser <abner.pelisser@gmail.com>
2022-08-21 20:46:43 -0300

    configurando o authorizarion code grant type - video 22.18

--- src/main/java/com/algafood/auth/AuthorizationServerConfig.java
+++ src/main/java/com/algafood/auth/AuthorizationServerConfig.java
@@ -35,6 +35,12 @@ public class AuthorizationServerConfig extends AuthorizationServerConfigurerAdap
 				.accessTokenValiditySeconds(6 * 60 * 60) // 6 horas
 				.refreshTokenValiditySeconds(60 * 24 * 60 * 60) // 60 dias
 			.and()
+				.withClient("algafood-analytics")
+				.secret(passwordEncoder.encode("analytics123456"))
+				.authorizedGrantTypes("authorization_code")
+				.scopes("write", "read")
+				.redirectUris("http://aplicacao-cliente")
+			.and()
 				.withClient("faturamento")
 				.secret(passwordEncoder.encode("faturamento123456"))
 				.authorizedGrantTypes("client_credentials")
