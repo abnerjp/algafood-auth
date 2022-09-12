202ad0f885f8bedb479ff4a59c256d8966f5051b
Abner J Pelisser <abner.pelisser@gmail.com>
2022-09-11 22:46:19 -0300

    carregando as permissoes concedidas na autenticacao - video 23.19

--- src/main/java/com/algafood/auth/domain/Grupo.java
+++ src/main/java/com/algafood/auth/domain/Grupo.java
@@ -3,7 +3,6 @@ package com.algafood.auth.domain;
 import java.util.HashSet;
 import java.util.Set;
 
-import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
@@ -15,30 +14,23 @@ import javax.persistence.ManyToMany;
 import lombok.Data;
 import lombok.EqualsAndHashCode;
 
-
 @Data
 @EqualsAndHashCode(onlyExplicitlyIncluded = true)
 @Entity
-public class Usuario {
+public class Grupo {
 
 	@EqualsAndHashCode.Include
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
 	
-	@Column(nullable = false)
 	private String nome;
 	
-	@Column(nullable = false)
-	private String email;
-	
-	@Column(nullable = false)
-	private String senha;
-	
 	@ManyToMany
-	@JoinTable(name = "usuario_grupo"
-	, joinColumns = @JoinColumn(name = "usuario_id")
-	, inverseJoinColumns = @JoinColumn(name = "grupo_id"))
-	private Set<Grupo> grupos = new HashSet<>();
-
+	@JoinTable(name = "grupo_permissao"
+	, joinColumns = @JoinColumn(name = "grupo_id")
+	, inverseJoinColumns = @JoinColumn(name = "permissao_id"))
+	private Set<Permissao> permissoes = new HashSet<>();
+	
+	
 }
